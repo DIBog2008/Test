c4d231f866295393155b253d83add76f4df3ec04
DIBog2008 <DIBog2008@gmail.com>
2026-09-06 00:35:26 +0300

    Калькудятор обновление

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <clocale>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
-int len(int a) {
-    if (a / 1000 > 0) {
-        return 0;
-    } else {
-        if (a / 100 > 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-}
+
 
 int main() {
-    std :: setlocale(LC_ALL, "Russian");
-    int a;
-    std :: cin >> a;
-    if (len(a)==1) {
-        std :: cout << "YES";
-    } else {
-        std :: cout << "NO";
+    int a,b;
+    char c;
+    std::cin >> a >> b >> c;
+    switch (c) {
+        case '+':
+            std::cout << a + b;
+            break;
+        case '-':
+            std::cout << a - b;
+            break;
+        case '*':
+            std::cout << a * b;
+            break;
+        case '/':
+            if (b == 0) {
+                std::cout << "На ноль делить нельзя!";
+                break;
+            }   else {
+                std::cout << a / b;
+                break;
+            }
+        default:
+            std::cout << "Неверная операция";
     }
-    return 0;
 }
