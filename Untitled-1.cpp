81a7037ec596d4c9cd75e38cbefa7d8b8036dcab
DIBog2008 <DIBog2008@gmail.com>
2026-09-05 20:51:41 +0300

    Невероятное изменение

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,18 +2,26 @@
 #include <clocale>
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
-
+int len(int a) {
+    if (a / 1000 > 0) {
+        return 0;
+    } else {
+        if (a / 100 > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
 
 int main() {
     std :: setlocale(LC_ALL, "Russian");
-    std :: cout << "Hello";
-    short a;
+    int a;
     std :: cin >> a;
-    if (a >= 12) {
-        std :: cout << "Доступ разрешен";
+    if (len(a)==1) {
+        std :: cout << "YES";
     } else {
-        std :: cout << "Доступ запрещен";
+        std :: cout << "NO";
     }
-
     return 0;
 }
