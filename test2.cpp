1c044eb612e831fadee60397c1f20d82eda2b6e6
DIBog2008 <DIBog2008@gmail.com>
2026-09-07 01:01:17 +0300

    Hello, I from Linux

---
+++ test2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <clocale>
+
+
+
+int main() {
+    int a,b;
+    char c;
+    std :: cout << "Hello";
+    std::cin >> a >> b >> c;
+    switch (c) {
+        case '+':
+            std::cout << a + b;
+            break;
+        case '-':
+            std::cout << a - b;
+            break;
+        case '*':
+            std::cout << a * b;
+            break;
+        case '/':
+            if (b == 0) {
+                std::cout << "На ноль делить нельзя!";
+                break;
+            }   else {
+                double c = static_cast<double>(a) / b;
+                std::cout << c;
+                break;
+            }
+        default:
+            std::cout << "Неверная операция";
+    }
+}
