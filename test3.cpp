97b20ce7dd51c7ff009d67fd0f4aacf09b809c36
DIBog2008 <DIBog2008@gmail.com>
2026-09-24 02:15:00 +0300

    Добавил удаление букв h and l

--- test3.cpp
+++ test3.cpp
@@ -3,6 +3,18 @@ int main() {
     int a;
     std::cin >> a;
     char word[a];
+    char new_word[a];
     std::cin >> word;
-    std::cout << word;
+    char* ptr = word;
+    char* ptr_n = new_word;
+    while (*ptr != '\0'){
+        if (*ptr != 'h' && *ptr != 'l'){
+            *ptr_n = *ptr;
+            ptr_n++;
+        }
+        ptr++;
+    }
+    *ptr_n = '\0';
+    ptr_n = new_word;
+    std::cout << new_word << '\n';
 }
