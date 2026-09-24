61c8784fc5602a5bbe537bb6e46978e7bb2d4a44
DIBog2008 <DIBog2008@gmail.com>
2026-09-24 03:00:00 +0300

    Добавлен вывод адреса 2

--- test3.cpp
+++ test3.cpp
@@ -11,13 +11,17 @@ int main() {
     int count = 0;
     const char** ptr = messages;
     while (count != size){
-        std:: cout << "Adress: " << ptr << " -> : text " << *ptr << '\n';
-        const char* gost = *ptr;
-        *ptr = *(ptr + (size - count));
-        *(ptr + (size - count) * 2) = gost;        
+        std:: cout << "Adress: " << ptr << " -> : text " << *ptr << '\n';  
         ptr++; 
         count++;
     }
+    count = 0;
+    ptr = messages;
+    const char* gost = *ptr;    
+    *ptr = *(ptr + 4);
+    *(ptr + 4) = gost;
+    gost = *(ptr + 3);
+    *(ptr + 1) = *(ptr + 3);
     count = 0;
     ptr = messages;
     while (count != size){
